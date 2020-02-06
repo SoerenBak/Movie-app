8b31131f928999b4a23dee2eaa2a7377f6aba4f1
SoerenBak <34088652+SoerenBak@users.noreply.github.com>
2020-02-06 09:16:03 +0100

    start

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav>
+            <div className="nav-wrapper container">
+                <a href="" className="brand-logo">Movie Finder</a>
+            </div>
+        </nav>
+    )
+}
